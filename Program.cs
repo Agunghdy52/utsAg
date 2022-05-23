f3f4f06fff4b36f08103d9541fda876fcf5897d0
Agunghy52 <agunghidayat2@sudents.amikom.ac.id>
2022-05-24 00:09:57 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utsAg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen1 = new Dosen(1, "Agung Hidayat", 21114452, "Laki-laki", "Pemrograman");
+
+            dosen1.cetakHasil();
+            Console.WriteLine();
+            dosen1.addDosen();
+            dosen1.addCourse();
+            Console.WriteLine();
+            dosen1.cetakAkhir();
+
+            Console.ReadKey();
+        }
+    }
+}
